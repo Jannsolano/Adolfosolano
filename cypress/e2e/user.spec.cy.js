7884c4ea334d00c3ddec343a48acdd162c6fdcf3
Jannsolano <jann.solano@gmail.com>
2025-06-17 17:27:46 -0300

    testing with random gen data

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import LoginPage from '../pages/LoginPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
+
+var chance= new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -13,10 +16,13 @@ describe('user tests', () => {
   it('User Info Update - sucess', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+
     dashboardPage.dashboardConfirm()
+
     menuPage.accessMyInfo()
-    myInfoPage.usernameChanges(userData.userInfo.firstName, userData.userInfo.secondName,userData.userInfo.lastName)
-    myInfoPage.userIdChanges(userData.userInfo.employeeID, userData.userInfo.otherID, userData.userInfo.driverLicense)
+
+    myInfoPage.usernameChanges(chance.first(), userData.userInfo.secondName, chance.last())
+    myInfoPage.userIdChanges(userData.userInfo.employeeID, chance.cpf(), userData.userInfo.driverLicense)
     myInfoPage.userOtherInfo(userData.userInfo.licenseExpDate, userData.userInfo.birthDate)
   })
 })
